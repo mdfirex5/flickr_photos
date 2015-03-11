f22e7b9b38ff5d369529301dea5ce7909225c54b
Sergei Sinyak <serega.belarus@gmail.com>
2015-03-11 17:22:05 +0000

    add gallery controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'gallery/index'
+
+  get 'gallery/search'
+
+  get 'gallery/show'
+
+  #root 'gallery#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
